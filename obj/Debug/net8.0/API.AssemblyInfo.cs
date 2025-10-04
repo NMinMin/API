7652ba7dcdd114fda7fe4d53badc8492c76da48a
Võ Nhạc Phước <vonhacphuoc@gmail.com>
2025-10-04 10:31:11 +0700

    Rename namespace

--- obj/Debug/net8.0/API.AssemblyInfo.cs
+++ obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d2235a008de325b4389179f592175576b645109")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9de5bd5bacfa133e02b89bdcca0daaca104b96ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
