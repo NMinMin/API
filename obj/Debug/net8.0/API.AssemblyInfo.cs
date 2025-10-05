ec92b46c75353593e7bb37f71675c8b35f9bf3d2
Võ Nhạc Phước <vonhacphuoc@gmail.com>
2025-10-05 20:41:52 +0700

    thêm dockerfile

--- obj/Debug/net8.0/API.AssemblyInfo.cs
+++ obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68fc94b6dfb8e44b494555b67d7957f549f76e4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ec2cafbb0fee1e5fe2cc1ebf909516fc312222c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
