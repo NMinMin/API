60dbc43c5299b2256c5bb94a9535b62838911cb8
Võ Nhạc Phước <vonhacphuoc@gmail.com>
2025-10-06 13:07:06 +0700

    ...

--- obj/Debug/net8.0/API.AssemblyInfo.cs
+++ obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68fc94b6dfb8e44b494555b67d7957f549f76e4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cbbeee7d35d7ee685688566360ab2bb75c04d99")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
