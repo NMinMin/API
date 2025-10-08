7569d47ab69002751b7e0638f2556af2647097de
VindWeen <fw62262@gmail.com>
2025-10-09 00:51:16 +0700

    .

--- obj/Debug/net8.0/API.AssemblyInfo.cs
+++ obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13b6b12c69fcf7b32a8ace5986465a027631a082")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9725389ae82b9860c32c7ade7af0bfe6124aaf8")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
