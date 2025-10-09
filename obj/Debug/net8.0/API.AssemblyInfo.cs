47183f87f7b32c240000823788eb87001c0334c9
VindWeen <fw62262@gmail.com>
2025-10-09 21:08:30 +0700

    update Class.cs; ClassController.cs

--- obj/Debug/net8.0/API.AssemblyInfo.cs
+++ obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7ab967942ca1cd0801be3e5ee8f8f926ffa765f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38bcc32881193e5b0c94a2aca52426245601d605")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
